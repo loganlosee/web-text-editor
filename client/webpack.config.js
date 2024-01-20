3e872bed126b173f0cc5b3de9dfb30c733f4c2ca
Logan Losee <logan.losee99@gmail.com>
2024-01-20 14:57:44 -0700

    webpack.config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,48 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: 'src/index.html', // Path to your HTML template
+        title: 'JATE'
+      }),
+      new InjectManifest({
+        swSrc: './src/src-sw.js', // Path to your source service worker file
+        swDest: 'service-worker.js',
+      }),
+      new WebpackPwaManifest({
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'A simple text editor',
+        background_color: '#ffffff',
+        theme_color: '#555555',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('client/src/images/logo.png'), 
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
       ],
     },
   };
